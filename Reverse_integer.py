6fc3e82061b81e49654139a8058356fdf19a40ba
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2021-01-15 00:02:28 -0700

    1.14.21.final

--- Reverse_integer.py
+++ Reverse_integer.py
@@ -1,30 +1,51 @@
-class Solution(object):
-    def __init__(self, x):
-        self.x = x
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the
+signed 32-bit integer range [-231, 231 - 1], then return 0.
 
-    def reverse(self, x):
-        self.x = x
-        """
-        :type x: int
-        :rtype: int
-        """
-        if not (-2 ** 31 <= x <= 2 ** 31 - 1):
-            return 0
-        else:
-            my_string = str(x)
-        if x >= 0:
-            my_string_reversed = my_string[::-1]
-        else:
-            temp = my_string[1:]
-            temp2 = temp[::-1]
-            my_string_reversed = "-" + temp2
-        if not (-2 ** 31 <= int(my_string_reversed) <= 2 ** 31 - 1):
-            return 0
-        else:
-            return int(my_string_reversed)
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+Example 4:
+
+Input: x = 0
+Output: 0
+
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+
+
+def reverse(x):
+    if not (-2 ** 31 <= x <= 2 ** 31 - 1):
+        return 0
+    else:
+        my_string = str(x)
+    if x >= 0:
+        my_string_reversed = my_string[::-1]
+    else:
+        temp = my_string[1:]
+        temp2 = temp[::-1]
+        my_string_reversed = "-" + temp2
+    if not (-2 ** 31 <= int(my_string_reversed) <= 2 ** 31 - 1):
+        return 0
+    else:
+        return int(my_string_reversed)
 
 
 my_number = int(input('Integer'))
-my_solution = Solution
-my_solution.reverse(my_number)
-print(my_solution)
+print(reverse(my_number))
