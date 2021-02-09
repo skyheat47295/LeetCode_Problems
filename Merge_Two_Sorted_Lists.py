6ead77fc66fc001413664741986783c3d5b1db62
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2021-02-08 19:31:44 -0700

    2.8.21.4

--- Merge_Two_Sorted_Lists.py
+++ Merge_Two_Sorted_Lists.py
@@ -49,7 +49,6 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-
         new_list = new_list_result = ListNode()
         while l1 and l2:
             if l1.val > l2.val:
@@ -67,13 +66,13 @@ class Solution:
         return new_list_result.next
 
 
-my_l1 = []
-# my_l1.next = ListNode(2)
-# my_l1.next.next = ListNode(4)
+my_l1 = ListNode(1)
+my_l1.next = ListNode(2)
+my_l1.next.next = ListNode(4)
 
-my_l2 = [0]
-# my_l2.next = ListNode(3)
-# my_l2.next.next = ListNode(4)
+my_l2 = ListNode(1)
+my_l2.next = ListNode(3)
+my_l2.next.next = ListNode(4)
 solution = Solution()
 
 print(solution.mergeTwoLists(my_l1, my_l2))
