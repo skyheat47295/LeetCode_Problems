cf5ffc29605ff9573696a48ee56e5bec70091aaf
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2021-02-25 22:52:24 -0700

    2.25.21

--- Climbing_Stairs.py
+++ Climbing_Stairs.py
@@ -40,3 +40,12 @@ Constraints:
 
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+
+solution = Solution()
+print(solution.climbStairs(38))
