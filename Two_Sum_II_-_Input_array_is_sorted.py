aa9883057830d869e2582d137829f9ec5b98e86e
skyheat47295 <70936076+skyheat47295@users.noreply.github.com>
2021-05-25 17:42:31 -0700

    5.25.21

---
+++ Two_Sum_II_-_Input_array_is_sorted.py
@@ -0,0 +1,58 @@
+"""167. Two Sum II - Input array is sorted
+Easy
+
+2714
+
+737
+
+Add to List
+
+Share
+Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+
+Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution."""
+
+
+class Solution:
+    def twosum(self, numbers: list[int], target: int) -> list[int]:
+        index = []
+        for number in range(len(numbers)):
+            for x in range(number + 1, len(numbers)):
+                if numbers[number] + numbers[x] == target:
+                    index.append(number + 1)
+                    index.append(x + 1)
+                    return index
+
+
+my_numbers = [2,7,11,15]
+my_target = 9
+solution = Solution()
+print(solution.twosum(my_numbers, my_target))
+#  This solution worked, but timed out. Let's make it more efficient.
